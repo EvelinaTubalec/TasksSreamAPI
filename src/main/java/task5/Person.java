c153e15576f87b786f9a566eebfd8a51671efe90
evelina.tubalets <evelina.tubalec@gmail.com>
2021-12-27 16:12:23 +0300

    fix Person

--- src/main/java/task5/Person.java
+++ src/main/java/task5/Person.java
@@ -20,24 +20,23 @@ public class Person {
     public Person(Long personId, String name, Skill... skills) {
         this.personId = personId;
         this.name = name;
-        this.skills = Arrays.asList(skills); }
+        this.skills = Arrays.asList(skills);
+    }
 
     public static void findBestMatchingPerson(List<Person> persons, Skill... skills) {
         Map<String, String> result = new HashMap<>();
-        Arrays.stream(skills).map(Skill::getName).forEach(name -> persons.forEach(p -> {
-            List<Skill> personSkills = p.getSkills();
-            personSkills.forEach(skill -> {
-                if (skill.getName().equals(name) && skill.getKnownPercentage() > 50) {
-                    result.put(name, p.getName());
-                } else if (skill.getName().equals(name)) {
-                    //[username=knownPercentage] use for specific skill
-                    Map<String, Long> allPercentageOfSkill = new HashMap<>();
+        Map<String, Long> allPercentageOfSkill = new HashMap<>();
+
+        Arrays.stream(skills).map(Skill::getName).forEach(name -> {
+            persons.forEach(p -> {
+                List<Skill> personSkills = p.getSkills();
+                personSkills.stream().filter(skill -> skill.getName().equals(name)).forEach(skill -> {
                     allPercentageOfSkill.put(p.getName(), skill.getKnownPercentage());
                     Map.Entry<String, Long> stringLongEntry = allPercentageOfSkill.entrySet().stream().max(Map.Entry.comparingByValue()).get();
                     result.put(name, stringLongEntry.getKey());
-                }
+                });
             });
-        }));
+        });
         System.out.println(result);
     }
 }
