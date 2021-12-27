928070957416786bc59b0bb42066c5ced67dc38e
evelina.tubalets <evelina.tubalec@gmail.com>
2021-12-27 10:39:05 +0300

    task5 with Stream API

--- src/main/java/task5/Person.java
+++ src/main/java/task5/Person.java
@@ -24,22 +24,20 @@ public class Person {
 
     public static void findBestMatchingPerson(List<Person> persons, Skill... skills) {
         Map<String, String> result = new HashMap<>();
-        Arrays.stream(skills).map(Skill::getName).forEach(name -> {
-            persons.forEach(p -> {
-                List<Skill> personSkills = p.getSkills();
-                personSkills.forEach(skill -> {
-                    if (skill.getName().equals(name) && skill.getKnownPercentage() > 50) {
-                        result.put(name, p.getName());
-                    } else if (skill.getName().equals(name)) {
-                        //[username=knownPercentage] use for specific skill
-                        Map<String, Long> allPercentageOfSkill = new HashMap<>();
-                        allPercentageOfSkill.put(p.getName(), skill.getKnownPercentage());
-                        Map.Entry<String, Long> stringLongEntry = allPercentageOfSkill.entrySet().stream().max(Map.Entry.comparingByValue()).get();
-                        result.put(name, stringLongEntry.getKey());
-                    }
-                });
+        Arrays.stream(skills).map(Skill::getName).forEach(name -> persons.forEach(p -> {
+            List<Skill> personSkills = p.getSkills();
+            personSkills.forEach(skill -> {
+                if (skill.getName().equals(name) && skill.getKnownPercentage() > 50) {
+                    result.put(name, p.getName());
+                } else if (skill.getName().equals(name)) {
+                    //[username=knownPercentage] use for specific skill
+                    Map<String, Long> allPercentageOfSkill = new HashMap<>();
+                    allPercentageOfSkill.put(p.getName(), skill.getKnownPercentage());
+                    Map.Entry<String, Long> stringLongEntry = allPercentageOfSkill.entrySet().stream().max(Map.Entry.comparingByValue()).get();
+                    result.put(name, stringLongEntry.getKey());
+                }
             });
-        });
+        }));
         System.out.println(result);
     }
 }
